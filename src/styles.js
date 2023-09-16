1fc2ccc1ef9eb79fe3a995c7b1c67f2e67b7620a
HamzaButt98 <m.hamza2456@gmail.com>
2023-09-16 16:04:23 +0500

    card changes

--- src/styles.js
+++ src/styles.js
@@ -3,6 +3,18 @@ const styles = {
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
+  '@media (min-width: 768px)': {
+    paddingX: "px-20",
+    paddingY: "py-8",
+    padding: "px-20 py-12",
+  },
+
+  '@media (min-width: 900px)': {
+    paddingX: "px-28",
+    paddingY: "py-10",
+    padding: "px-28 py-16",
+  },
+
   heroHeadText:
     "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
